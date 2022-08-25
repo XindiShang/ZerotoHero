3522fe43e5063a0f0d793dac58c9f1088b41ec9e
sxd <shangxindi@gmail.com>
2022-08-25 16:53:12 +0800

    finish react stream project

--- React-Practice-and-Notes/my-app/src/9. Stream/client/components/streams/StreamShow.jsx
+++ React-Practice-and-Notes/my-app/src/9. Stream/client/components/streams/StreamShow.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
@@ -7,8 +8,28 @@ const StreamShow = (props) => {
   const { fetchStream } = props;
   const { title, description } = props.stream;
 
+  const videoRef = useRef(null); // ref => { current: null }
+  let player;
+
+  const createPlayer = () => {
+    if (player || !videoRef.current) return;
+      
+    player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+
+    player.attachMediaElement(videoRef.current);
+    player.load();
+  }
+
   useEffect(() => {
     fetchStream(id);
+    createPlayer();
+    return () => {
+      // Anything in here will run when the component unmounts
+      player.destroy()
+    }
   }, [id, fetchStream]);
 
   const renderContent = () => {
@@ -17,6 +38,7 @@ const StreamShow = (props) => {
     }
     return (
       <div>
+        <video ref={videoRef} style={{width: '100%'}} controls />
         <h3>{title}</h3>
         <h4>{description}</h4>
         <hr />
