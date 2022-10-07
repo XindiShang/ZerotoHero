8913e0390c4cc6e53c5fe8a40303e2fb46a8a671
Addison <shangxindi@gmail.com>
2022-10-07 20:43:42 +0800

    complete login page api

--- Facebook-Clone/client/src/App.js
+++ Facebook-Clone/client/src/App.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Profile from './pages/profile/Profile';
@@ -6,15 +7,19 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate,
 } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
+  console.log(user)
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={user? <Home /> : <Register/>} />
+        <Route path="/login" element={user? <Navigate replace to="/" /> : <Login />} />
+        <Route path="/register" element={user? <Navigate replace to="/" /> : <Register />} />
         <Route path="/profile/:userName" element={<Profile />} />      
       </Routes>
     </Router>
--- Facebook-Clone/client/src/pages/login/Login.jsx
+++ Facebook-Clone/client/src/pages/login/Login.jsx
@@ -1,8 +1,24 @@
-import './login.scss';
+import "./login.scss";
+import { useRef, useContext } from "react";
+import { login } from "../../api";
+import { AuthContext } from "../../context/AuthContext";
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Login = () => {
+  const email = useRef();
+  const password = useRef();
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(
+      { email: email.current.value, password: password.current.value },
+      dispatch
+    );
+  };
+
   return (
-    <div className='login'>
+    <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
           <h3 className="loginLogo">Faceybook</h3>
@@ -11,17 +27,31 @@ const Login = () => {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <button className="loginButton">Log In</button>
+          <form className="loginBox" onSubmit={handleSubmit}>
+            <input
+              ref={email}
+              required
+              placeholder="Email"
+              type="email"
+              className="loginInput"
+            />
+            <input
+              ref={password}
+              minLength="6"
+              placeholder="Password"
+              type="password"
+              className="loginInput"
+            />
+            <button className="loginButton" disabled={isFetching} >{isFetching ? <CircularProgress sx={{ color: 'white' }} size="20px" /> : 'Log In'}</button>
             <span className="loginForgot">Forgot Password?</span>
-            <button className="loginRegisterButton">Create a New Account</button>
-          </div>
+            <button className="loginRegisterButton">
+              Create a New Account
+            </button>
+          </form>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Login;
